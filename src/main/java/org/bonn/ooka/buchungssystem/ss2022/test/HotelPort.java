d3bc8ac7899fd10544208e36564ba1d262cb8fbc
erik <e-aut@web.de>
2023-04-16 13:50:37 +0200

    - Add UML

--- src/main/java/org/bonn/ooka/buchungssystem/ss2022/test/HotelPort.java
+++
@@ -1,9 +0,0 @@
-package org.bonn.ooka.buchungssystem.ss2022.test;
-
-import org.bonn.ooka.buchungssystem.ss2022.Hotel;
-
-import java.util.List;
-
-public interface HotelPort {
-    public List<Hotel> searchHotels(String keyword);
-}
